2dfaf8051a600039c33d6b922adfc121d8e4abc9
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-28 09:59:03 +0200

    add typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Suspense } from "react";
 import Loading from "./loading";
-// import styles from "./globals.module.scss";
+import "./styles/globals.scss";
 
 export const metadata: Metadata = {
   title: "SHOPPE",
