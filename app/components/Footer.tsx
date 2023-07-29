544a32b4df6b754888c0d0efbea8b6c4b49f52a4
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-29 19:01:31 +0200

    add checkbox for terms to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -49,7 +49,37 @@ const Footer = () => {
               </svg>
             </div>
           </div>
-          <SocialSVG />
+          <div className={styles["checkbox-wrapper"]}>
+            <input id="check1-61" className={styles.check} type="checkbox" />
+            <label className={styles.label} htmlFor="check1-61">
+              <svg viewBox="0 0 95 95" height="45" width="45">
+                <rect
+                  fill="none"
+                  stroke="black"
+                  height="50"
+                  width="50"
+                  y="20"
+                  x="30"
+                ></rect>
+                <g transform="translate(0,-952.36222)">
+                  <path
+                    className={styles.path1}
+                    fill="none"
+                    strokeWidth="3"
+                    stroke="black"
+                    d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4"
+                  ></path>
+                </g>
+              </svg>
+            </label>
+            <span className={styles.checkboxText}>
+              I agree to the Terms & Conditions
+            </span>
+          </div>
+          <div className={styles.footerSocialText}>
+            <SocialSVG />
+            <p>Follow us on social media</p>
+          </div>
         </div>
       </footer>
       <div className={styles.footerLegal}>
