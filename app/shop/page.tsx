0eabb1f28dfeaf57930e19d217437966303777e5
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-31 22:28:10 +0200

    add contetn to shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,11 +1,23 @@
+import styles from "../styles/pages/shop.module.scss";
 import React from "react";
-
+import Image from "next/image";
 const Shop = () => {
   return (
-    <div>
-      <h1>Shop</h1>
-      <h1>Coming soon!</h1>
-    </div>
+    <>
+      <div className={styles.container}>
+        <h1>Shop Page</h1>
+      </div>
+      <div className={styles.container}>
+        <h1>Coming soon!</h1>
+      </div>
+      <Image
+        src="/our-story.jpg"
+        alt="Coming Soon"
+        width={500}
+        height={750}
+        className={styles["img"]}
+      />
+    </>
   );
 };
 
