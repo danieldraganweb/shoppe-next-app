5b1671c7c5941d32cb5be99f5020b92ac1a61b81
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-08-01 15:58:07 +0200

    fix banner error mobile

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -63,19 +63,12 @@ export function BannerHero() {
                 height={banner.fields?.Image[0].height}
                 priority={true}
                 layout="responsive"
+                objectFit="cover"
                 style={{
                   // boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
                   borderRadius: "0.2em",
                   padding: "0 0.5em 0 0.5em",
                 }}
-                // className={` ${styles["transition-opacity"]} ${
-                //   styles["opacity-0"]
-                // } ${styles["transition-timing-function"]} ${
-                //   styles["duration-300"]
-                // } ${styles["ease-in-out"]}
-                //         ${
-                //           loading ? styles["opacity-0"] : styles["opacity-100"]
-                //         }`}
               />
             </div>
           ))}
